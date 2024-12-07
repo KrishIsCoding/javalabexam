5f185838902b6e4b2cf7ff31ba222a4da1ef3d59
KrishIsCoding <gautamkrish445@gmail.com>
2024-12-07 12:41:38 +0530

    Add files via upload

---
+++ ControllerAssesment.java
@@ -0,0 +1,24 @@
+package com.assesment.assesment;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.assesment.assesment.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class ControllerAssesment {
+
+	 CalculateModel calculatemodel = new CalculateModel();
+	 
+	 @Autowired
+	 private ServiceAssesment serviceAssesment;
+	 
+	
+	 @PostMapping(value="/add")
+         public int calculateSum(@RequestBody CalculateModel calculatemodel) {			
+	 return  serviceAssesment.add(calculatemodel);
+	}
+}
